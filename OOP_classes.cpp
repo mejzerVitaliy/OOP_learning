ed873b7f470e79b53beafcd5ab7728025da937c3
mejzerVitaliy <vitalii.mejzer@lumitech.co>
2025-02-09 23:15:05 +0200

    Добавьте файлы проекта.

---
+++ OOP_classes.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+class Numbers {
+public:
+    int first_num;
+    int second_num;
+
+    void set(int first, int second)
+    {
+        first_num = first;
+        second_num = second;
+    }
+
+    void print()
+    {
+        std::cout << "Numbers(" << first_num << ", " << second_num << ")\n";
+    }
+};
+
+int main()
+{
+    Numbers n1;
+    n1.set(314, 341);
+    
+    Numbers n2{ 214, 442 };
+
+    n1.print();
+    n2.print();
+
+    return 0;
+}
